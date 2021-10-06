e5afc7bb7597f49be86e8e118494f2d7ec6f3c9f
Nelson Berrio <nberrio1996@gmail.com>
2021-10-06 17:42:12 -0500

    Se crea metodo get y post  para productos

---
+++ server.js
@@ -0,0 +1,23 @@
+import Express from "express"; 
+
+const app = Express()
+
+app.use(Express.json());
+
+app.get('/productos', (req, res)=> {
+    console.log('Alguien hizo get en la ruta /productos');
+    const productos = [
+        {idproducto : '1010', descripcion : 'Adidas Stan Smith', valorunitario : 250000, estado : 'No Disponible'},
+        {idproducto: '2020', descripcion : 'Adidas Ultra Bost', valorunitario : 265000, estado : 'Disponible'},
+        {idproducto: '3030', descripcion : 'Nike Stefan Janoski', valorunitario : 300000, estado : 'No Disponible'},
+    ];
+    res.send(productos);
+});
+
+app.post('/productos/nuevo', (req, res) =>{
+    console.log('Alguien hizo post en la ruta /productos/nuevo');
+});
+
+app.listen(5000,()=>{
+    console.log('escuchando puerto 5000')
+});
