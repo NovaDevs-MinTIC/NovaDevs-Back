5ac891d670707dc1532cab0f72f17c2fa53bb273
Oscar <os.gomez.lozano@gmail.com>
2021-10-09 15:32:28 -0500

    Creacion de las carpetas del back

---
+++ views/productos/rutas.js
@@ -0,0 +1,9 @@
+import Express from 'express';
+import {
+    queryListaProducto,
+    crearProducto,
+    eliminarProducto,
+    editarProducto,
+    consultarProducto,
+  } from '../../controllers/producto/controller.js';
+  
