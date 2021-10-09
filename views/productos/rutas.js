31afaefb289f227e119f5f2e2f6c9820cd8406cb
Oscar <os.gomez.lozano@gmail.com>
2021-10-09 15:56:08 -0500

    configuración rutas.js

--- views/productos/rutas.js
+++ views/productos/rutas.js
@@ -5,5 +5,38 @@ import {
     eliminarProducto,
     editarProducto,
     consultarProducto,
-  } from '../../controllers/producto/controller.js';
+} from '../../controllers/producto/controller.js';
   
+const rutasVehiculo = Express.Router();
+
+const genercCallback = (res) => (err, result) => {
+  if (err) {
+    res.status(500).send('Error consultando los productos');
+  } else {
+    res.json(result);
+  }
+};
+
+rutasProducto.route('/productos').get((req, res) => {
+  console.log('alguien hizo get en la ruta /productos');
+  queryListaProducto(genercCallback(res));
+});
+
+rutasProducto.route('/productos').post((req, res) => {
+  crearProducto(req.body, genercCallback(res));
+});
+
+rutasProducto.route('/productos/:id').get((req, res) => {
+  console.log('alguien hizo get en la ruta /productos');
+  consultarProducto(req.params.id, genercCallback(res));
+});
+
+rutasProducto.route('/productos/:id').patch((req, res) => {
+  editarProducto(req.params.id, req.body, genercCallback(res));
+});
+
+rutasProducto.route('/productos/:id').delete((req, res) => {
+  eliminarProducto(req.params.id, genercCallback(res));
+});
+
+export default rutasProducto;
