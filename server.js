e2add51eb53ff35285ea5c19a9fe5887d5f1f588
Nelson Berrio <nberrio1996@gmail.com>
2021-10-22 20:04:38 -0500

    se agrega jwtCheck

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import { conectarBD } from './db/db.js';
 import rutasProducto from './views/productos/rutas.js';
 import rutasUsuario from './views/usuarios/rutas.js'
 import rutasVenta from './views/ventas/rutas.js'
+import jwt from 'express-jwt'
+import jwks from 'jwks-rsa'
 
 dotenv.config({ path: './.env' });
 
@@ -12,6 +14,20 @@ const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://novadevs.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-autenticacion-novadevs',
+issuer: 'https://novadevs.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 app.use(rutasProducto);
 app.use(rutasUsuario);
 app.use(rutasVenta);
